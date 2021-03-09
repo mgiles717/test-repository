501611573e27179e11c6cadd49760a95cbf0bfb9
mgiles717 <51393622+mgiles717@users.noreply.github.com>
2021-03-09 02:52:17 +0000

    Add files via upload

---
+++ testProgram.cpp
@@ -0,0 +1,8 @@
+//Created by M Giles: 09/03/2021
+//Test C++ program to print Hello World for GitHub repository testing.
+#include iostream
+
+int main(){
+	std::cout << "Hello World!"
+	return 0;
+}
